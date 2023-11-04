15a2c012947236e8b1309da0d4de93d886883675
XuanDuy97 <xuanduy97@dnkinno.com>
2023-11-04 19:02:43 +0700

    Finish task

---
+++ lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class MainColor{
+  static Color primaryColor = Color(0xFFFF4B4B);
+  static Color secondaryColor = Color(0xFFFFCA27);
+  static Color accentColor = Color(0xFF4169E8 );
+}
